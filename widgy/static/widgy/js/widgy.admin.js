67689d14f6fce96fa45980c840c89735c5c7d694
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-11-25 20:40:55 -0700

    Problems with importing

--- widgy/static/widgy/js/widgy.admin.js
+++ widgy/static/widgy/js/widgy.admin.js
@@ -1,15 +1,3 @@
-define(function(require, exports, module) {
-require([
-  "node_modules/dialog-component/index",
-  "node_modules/dialog-component/components/component-emitter/index",
-  "node_modules/dialog-component/components/component-jquery/index",
-  "node_modules/dialog-component/components/component-overlay/index",
-  "node_modules/dialog-component/index"
-], function(dialog, emitter, jquery, overlay) {
-  dialog("Hello World");
-});
-});
-
 if ( typeof window.console == 'undefined' )
 {
   var $console = $('<ul class="console" style="margin-bottom: 100px"></ul>');
